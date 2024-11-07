7278c2fef47102f412bd6b44be798f8533bebfa7
devimadduri <130981474+devimadduri@users.noreply.github.com>
2024-11-07 08:30:48 +0530

    Initial commit

---
+++ Nov/EX-051120124_pytho_Basics/Lab001_Python_HelloWorld.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print("Devi",12345)
---
+++ Nov/EX-051120124_pytho_Basics/Lab003_Python_Basics.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Devi")
+#print(self, *args, sep=' ', end='\n', file=None):
+#print(, *args, sep=' ', end='\n', file=None):
+
+print("Devi",123,"Praveen","Reyansh","Agasthya")
+print("Devi",123,"Praveen","Reyansh","Agasthya",sep= '*',end="     "   )
+print("Devi",123,"Praveen","Reyansh","Agasthya",sep= '*')
