c815d1bb359dd275a4b3bf950fc39c6042f83449
MadduriDevi <madduridevi2@gmail.com>
2024-11-12 00:04:52 +0530

    Update Lab029_Task1.py

--- Nov/EX-09112024_Literals_Variables/Lab029_Task1.py
+++ Nov/EX-09112024_Literals_Variables/Lab029_Task1.py
@@ -1,3 +1,22 @@
 # Task
 # Take a 3 input from the user
 #Perform the sub,Add,mul,and div
+
+num1 = int(input("Enter the num 1 :"))
+num2 = int (input("Enter the num 2 :"))
+num3 = int (input("Enter the num 3 :"))
+
+sum = num1 + num2 + num3
+sub = num1 - num2 - num3
+mul = num1 * num2 * num3
+div = num1 / num2 / num3
+
+print("sum of 3 number: ", sum)
+print("sub of 3 number: ", sub)
+print("mul of 3 number: ", mul)
+print("div of 3 number: ", div)
+
+print(type(sum))
+print(type(sub))
+print(type(mul))
+print(type(div))
