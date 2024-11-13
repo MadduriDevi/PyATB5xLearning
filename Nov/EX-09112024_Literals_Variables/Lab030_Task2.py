5204e60d72b293235d0b28c08c2711c3b2a99fc2
devimadduri <130981474+devimadduri@users.noreply.github.com>
2024-11-13 16:54:58 +0530

    9th november part-2

--- Nov/EX-09112024_Literals_Variables/Lab030_Task2.py
+++ Nov/EX-09112024_Literals_Variables/Lab030_Task2.py
@@ -5,3 +5,17 @@
 # 2 ->  num2
 #Q -> 7
 # R -> 1
+
+num1 = int(input("Enter the num 1 :"))
+num2 = int (input("Enter the num 2 :"))
+div = num1/num2
+print("div of the two num: " , div)
+
+q = num1 // num2
+
+print(" quotient  : " , q)
+print(type(q))
+
+r = num1 % num2
+print("reminder  : " , r)
+print(type(r))
