c0d23316d731dc2caae037c3e8386a45e93e4e2f
devimadduri <130981474+devimadduri@users.noreply.github.com>
2024-12-06 15:10:44 +0530

    3rd December

---
+++ Dec/Ex-o3122024_OOPS_Inheritance/Lab141_Polymorphism.py
@@ -0,0 +1,13 @@
+
+# Method Overloading
+class Dog:
+    def bark(self):
+        print("Dog is Barking")
+
+    def bark(self, breed):
+        print(f"Dog with {breed} is barking")
+
+
+d = Dog()
+
+
