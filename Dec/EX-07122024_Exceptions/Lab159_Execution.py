c588499deeab697a222cdb32232223f7c40d865c
devimadduri <130981474+devimadduri@users.noreply.github.com>
2024-12-11 12:07:43 +0530

    7th December

---
+++ Dec/EX-07122024_Exceptions/Lab159_Execution.py
@@ -0,0 +1,3 @@
+
+result = 10 / 0 # ZeroDivisionError: division by zero
+
---
+++ Dec/EX-07122024_Exceptions/Lab175_Main_Method.py
@@ -0,0 +1,7 @@
+
+def main():
+    print("Main method")
+
+
+if __name__ == "__main__": #Python Int -> Main method is -> Line Main
+    main()
